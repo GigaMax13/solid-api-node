63a2526a60ec744ac69e36c8e75fce1b748cfc41
Maximiliano Dalla Porta <gigamax13@gmail.com>
2020-10-16 23:09:03 -0300

    feat: added the cross-env to use NODE_ENV
    added the cross-env to use NODE_ENV for the correct environment load

--- src/utils/Environment.ts
+++ src/utils/Environment.ts
@@ -2,6 +2,21 @@ import { config } from 'dotenv'
 
 config()
 
+let path
+
+switch (process.env.NODE_ENV) {
+  case 'test':
+    path = `${__dirname}/../../.env.test`
+    break
+  case 'production':
+    path = `${__dirname}/../../.env.production`
+    break
+  default:
+    path = `${__dirname}/../../.env.development`
+}
+
+config({ path })
+
 /*
   TODO
   find a better way to destruct the env object and export its value
